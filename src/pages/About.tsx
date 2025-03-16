7d144b1ff9d3e026e0c0291e1aef077cdc1440ef
Sahil Khan <sahilkhan123098p@gmail.com>
2025-03-16 14:50:31 +0530

    add sample pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About: React.FC = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Welcome to my portfolio! My name is Sahil Khan, and I am a passionate developer.</p>
+            <p>Here you can find more information about my skills, experience, and projects.</p>
+        </div>
+    );
+};
+
+export default About;
